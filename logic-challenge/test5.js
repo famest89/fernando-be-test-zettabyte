efff5b7f0018ce4659804fdd756008e01ab02934
fernando <famest89@gmail.com>
2025-06-17 02:21:04 +0700

    finish test5

--- logic-challenge/test5.js
+++ logic-challenge/test5.js
@@ -8,7 +8,17 @@
 const words = ['flower', 'flow', 'flight'];
 
 function result(words) {
-  // Your Code Here
+  if (words.length === 0) return '';
+
+  let prefix = words[0];
+
+  for (let i = 1; i < words.length; i++) {
+    while (words[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+    }
+  }
+
+  return prefix;
 }
 
 console.log(result(words));
