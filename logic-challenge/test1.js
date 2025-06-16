5d8c533029d444fa93c6e48f843da72a4d14b824
fernando <famest89@gmail.com>
2025-06-17 02:55:18 +0700

    test5

--- logic-challenge/test1.js
+++ logic-challenge/test1.js
@@ -1,7 +1,7 @@
 /**
  * Direction:
  * Return a formatted array of sessions with list of classes & students
- * 
+ *
  * Expected Result:
  * [
  *  {
@@ -52,18 +52,59 @@
  */
 
 const sessions = [
-  { session_id: 1, time: '09:00', student: { student_id: 1, name: 'Adi' }, class: { class_id: 1, name: 'A' } },
-  { session_id: 2, time: '10:00', student: { student_id: 5, name: 'Surya' }, class: { class_id: 3, name: 'C' } },
-  { session_id: 2, time: '10:00', student: { student_id: 8, name: 'Edi' }, class: { class_id: 4, name: 'D' } },
-  { session_id: 2, time: '10:00', student: { student_id: 7, name: 'Dede' }, class: { class_id: 4, name: 'D' } },
-  { session_id: 1, time: '09:00', student: { student_id: 3, name: 'Bayu' }, class: { class_id: 2, name: 'B' } },
-  { session_id: 1, time: '09:00', student: { student_id: 2, name: 'Budi' }, class: { class_id: 1, name: 'A' } },
-  { session_id: 1, time: '09:00', student: { student_id: 4, name: 'Dharma' }, class: { class_id: 2, name: 'B' } },
-  { session_id: 2, time: '10:00', student: { student_id: 3, name: 'Maha' }, class: { class_id: 3, name: 'C' } },
+  {
+    session_id: 1,
+    time: '09:00',
+    student: { student_id: 1, name: 'Adi' },
+    class: { class_id: 1, name: 'A' },
+  },
+  {
+    session_id: 2,
+    time: '10:00',
+    student: { student_id: 5, name: 'Surya' },
+    class: { class_id: 3, name: 'C' },
+  },
+  {
+    session_id: 2,
+    time: '10:00',
+    student: { student_id: 8, name: 'Edi' },
+    class: { class_id: 4, name: 'D' },
+  },
+  {
+    session_id: 2,
+    time: '10:00',
+    student: { student_id: 7, name: 'Dede' },
+    class: { class_id: 4, name: 'D' },
+  },
+  {
+    session_id: 1,
+    time: '09:00',
+    student: { student_id: 3, name: 'Bayu' },
+    class: { class_id: 2, name: 'B' },
+  },
+  {
+    session_id: 1,
+    time: '09:00',
+    student: { student_id: 2, name: 'Budi' },
+    class: { class_id: 1, name: 'A' },
+  },
+  {
+    session_id: 1,
+    time: '09:00',
+    student: { student_id: 4, name: 'Dharma' },
+    class: { class_id: 2, name: 'B' },
+  },
+  {
+    session_id: 2,
+    time: '10:00',
+    student: { student_id: 3, name: 'Maha' },
+    class: { class_id: 3, name: 'C' },
+  },
 ];
 
 function result(sessions) {
-  // Your Code Here
+  console.log(`Sorry time's up`);
+  console.log(`Maybe next time`);
 }
 
 console.log(result(sessions));
